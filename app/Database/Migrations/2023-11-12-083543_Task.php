d083bc5333d2602b003f7387ecde041e3b0aee4d
ryanf27 <ryanfebry0227@gmail.com>
2023-11-12 15:53:46 +0700

    first init for to do list app with codeigniter

---
+++ app/Database/Migrations/2023-11-12-083543_Task.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Task extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'title' => [
+                'type' => 'VARCHAR',
+                'constraint' => 225,
+            ],
+            'description' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'completed' => [
+                'type' => 'BOOLEAN',
+                'default' => false,
+            ],
+        ]);
+
+        $this->forge->addKey('id' , true);
+        $this->forge->createTable('tasks');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('tasks');
+    }
+}
